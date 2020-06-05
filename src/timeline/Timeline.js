37657d943b1ede3ac0fd7d61bdfd8ec38ccb9a38
Jamie Bradley <jamie@endeavour-digital.com>
2020-06-05 11:06:34 +0100

    Corrects issue with labels not appearing properly

--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -101,9 +101,7 @@ export default class Timeline extends React.PureComponent {
 
     return range(start, end + 1).map((i, index) => {
       let timeText;
-      if (i === start) {
-        timeText = '';
-      } else if (i < 12) {
+      if (i < 12) {
         timeText = !format24h ? `${i} AM` : `${i}:00`;
       } else if (i === 12) {
         timeText = !format24h ? `${i} PM` : `${i}:00`;
@@ -243,6 +241,7 @@ export default class Timeline extends React.PureComponent {
   render() {
     return (
       <ScrollView
+        style={{paddingTop: 40}}
         refreshControl={
           this.props.refreshCallback ? (
             <RefreshControl
